5abb4affe4520d5edef573a1ddbe3fe3b3ca4119
conor-v <conor.vanoystaeyen@student.howest.be>
2019-10-22 00:08:21 +0200

    content toggle gemaakt

--- src/index.js
+++ src/index.js
@@ -5,10 +5,10 @@ import data from './assets/data/coffees.json';
   const init = () => {
     getCoffees(data);
 
-    const $lis = document.querySelectorAll(`.price`);
-    for (let i = 0;i < $lis.length;i ++) {
-      $lis[i].addEventListener(`click`, addOrder);
-    }
+    const $orderItem = document.querySelector(`.order`);
+    const $ordersList = document.querySelector(`.orders`).contains($orderItem);
+    toggleContent($ordersList);
+
   };
 
   const getCoffees = data => {
@@ -22,41 +22,41 @@ import data from './assets/data/coffees.json';
 
   const makeCoffee = coffee => {
     const $lijst = document.querySelector(`.prices__list`);
-    //LI
+
     const $li = document.createElement(`li`);
     $li.classList.add(`price`);
     $lijst.appendChild($li);
-    //A
-    const $a = document.createElement(`a`);
-    $a.classList.add(`price__button`);
-    $li.appendChild($a);
-    //SPAN DIV
-    const $spanD = document.createElement(`span`);
-    $spanD.classList.add(`price__button__wrapper`);
-    $a.appendChild($spanD);
-    //SPAN NAME
-    const $spanN = document.createElement(`span`);
-    $spanN.classList.add(`price__button__name`);
-    $spanN.textContent = coffee.name;
-    $spanD.appendChild($spanN);
-    //SPAN PRIJS
-    const $spanP = document.createElement(`span`);
-    $spanP.classList.add(`price__button__amount`);
-    $spanP.textContent = `€ ${round(coffee.prices.medium)}`;
-    $spanD.appendChild($spanP);
-    //SPAN BUTTON
-    const $spanB = document.createElement(`span`);
-    $spanB.classList.add(`price__button__plus`);
-    $spanB.textContent = `+`;
-    $a.appendChild($spanB);
+
+    $li.innerHTML = opbouw(coffee);
+  };
+
+  const opbouw = data => {
+    return `
+    <a class="price__button">
+      <span class="price__button__wrapper">
+        <span class="price__button__name">${data.name}</span>
+        <span class="price__button__amount">&euro; ${round(data.prices.medium)}</span>
+      </span>
+      <span class="price__button__plus">+</span>
+    </a>`;
   };
 
   const round = num => {
     return num.toFixed(2);
   };
 
-  const addOrder = () => {
-    console.log(`click`);
+  const toggleContent = ulInfo => {
+    const $empty = document.querySelector(`.emptystate`);
+    const $notEmpty = document.querySelector(`.orders__wrapper`);
+
+    if (ulInfo === false) {
+      $empty.classList.remove(`hide`);
+      $notEmpty.classList.add(`hide`);
+    }
+    if (ulInfo === true) {
+      $empty.classList.add(`hide`);
+      $notEmpty.classList.remove(`hide`);
+    }
   };
 
   init();
